49e47d6aecd7e309a6909375e084f4923c273531
mendable <yanberdint@gmail.com>
2015-11-08 08:59:28 +0300

    add reply keyboard markup

--- lib/telegram_meetup_bot/messenger.rb
+++ lib/telegram_meetup_bot/messenger.rb
@@ -8,7 +8,17 @@ module TelegramMeetupBot
     end
 
     def send_text(text)
-      api.send_message(chat_id: chat_id, text: text)
+      api.send_message(chat_id: chat_id, text: text, reply_markup: keyboard)
+    end
+
+    private
+
+    def keyboard
+      Telegram::Bot::Types::ReplyKeyboardMarkup.new(
+        keyboard: [%w(/date /list /cal /cancel)],
+        one_time_keyboard: false,
+        resize_keyboard: true
+      )
     end
   end
 end
